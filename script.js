a48f8f20e4cb7d4b11d95ecb5dfd5837f41a7ca0
SinZ <osk0121@naver.com>
2025-10-09 16:34:34 +0900

    prevent caching

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (async function() {
     var src = '';
 
-    const ko = await fetch('./music/ko.csv').then((res) => res.text());
+    const ko = await fetch('./music/ko.csv?ts=' + Date.now()).then((res) => res.text());
     var head = '<tr class="head"><td>제목</td><td>금영</td><td>태진</td><td>JOYSOUND</td></tr>';
     ko.trim().split('\n').forEach((e, i) => {
         if (e.trim() == '') return;
