d4db85f8b66ed9d7e7859bcae72bda0f763804a7
SinZ <osk0121@naver.com>
2026-07-06 02:46:30 +0900

    mobile support

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 (async function() {
+
+    var s = (n) => n == '-' ? '<font color=GRAY>(미수록)</font>' : n;
+
     var src = '';
+    var src_mobile = ''
 
     const ko = await fetch('./music/ko.csv?ts=' + Date.now()).then((res) => res.text());
     var head = '<tr class="head"><td>제목</td><td>금영</td><td>태진</td><td>JOYSOUND</td></tr>';
     ko.trim().split('\n').forEach((e, i) => {
-        if (e.trim() == '') return;
+        e = e.trim();
+        if (e == '') return;
         if (e.includes(',,')) {
             e = e.split(',,');
             src += '<tr><td>' + e[0] + '</td><td>' + e[1] + '</td><td>' + e[2] + '</td><td>' + e[3] + '</td></tr>';
@@ -15,6 +20,21 @@
         }
     });
     src += '</table>';
+    ko.trim().split('\n').forEach((e, i) => {
+        e = e.trim();
+        if (e == '') return;
+        if (e.includes(',,')) {
+            e = e.split(',,');
+            src_mobile += '<tr class="head"><td colspan="2">' + e[0] + '</td></tr>';
+            src_mobile += '<tr><td style="width:50%">금영 : ' + s(e[1]) + '</td><td class="mid">태진 : ' + s(e[2]) + '</td></tr>';
+            src_mobile += '<tr><td colspan="2">JOYSOUND : ' + s(e[3]) + '</td></tr>';
+        } else {
+            if (i > 0) src_mobile += '</table>';
+            src_mobile += '<table>';
+            src_mobile += '<tr><th colspan="2">' + e + '</th></tr>';
+        }
+    });
+    src_mobile += '</table>';
 
     const ja = await fetch('./music/ja.csv').then((res) => res.text());
     src += '<table>';
@@ -31,6 +51,20 @@
         }
     });
     src += '</table>';
+    ja.trim().split('\n').forEach((e, i) => {
+        e = e.trim();
+        if (e == '') return;
+        if (e.includes(',,')) {
+            e = e.split(',,');
+            src_mobile += '<tr class="head"><td colspan="2">' + e[0] + '</td></tr>';
+            src_mobile += '<tr><td style="width:70%">JOYSOUND : ' + s(e[1]) + '</td><td class="mid">e-bo : ' + e[2] + '</td></tr>';
+        } else {
+            if (i > 0) src_mobile += '</table>';
+            src_mobile += '<table>';
+            src_mobile += '<tr><th colspan="2">' + e + '</th></tr>';
+        }
+    });
+    src_mobile += '</table>';
 
 
     for (var e of ['yerin', 'yuju', 'viviz', 'misc']) {
@@ -49,8 +83,23 @@
             }
         });
         src += '</table>';
+        music.trim().split('\n').forEach((e, i) => {
+            e = e.trim();
+            if (e == '') return;
+            if (e.includes(',,')) {
+                e = e.split(',,');
+                src_mobile += '<tr class="head"><td colspan="2">' + e[0] + '</td></tr>';
+                src_mobile += '<tr><td style="width:50%">금영 : ' + s(e[1]) + '</td><td class="mid">태진 : ' + s(e[2]) + '</td></tr>';
+            } else {
+                if (i > 0) src_mobile += '</table>';
+                src_mobile += '<table>';
+                src_mobile += '<tr><th colspan="2">' + e + '</th></tr>';
+            }
+        });
+        src_mobile += '</table>';
     };
 
-    document.getElementById('music_list').innerHTML = src;
+    document.getElementById('music_list_pc').innerHTML = src;
+    document.getElementById('music_list_mobile').innerHTML = src_mobile;
 
 })();
